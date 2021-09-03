7c2baa6f07d97809cfc24419e6bed9553c588902
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-02 20:50:39 -0400

    Write tests and corresponding code for creating a new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,9 +5,24 @@ class ApplicationsController < ApplicationController
   end
 
   def show
-    @application = Application.find(params[:id])
+    @application = Application.find(params[:application_id])
   end
 
   def new
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def create
+
+    pet = Pet.find(params[:pet_id])
+    application = pet.applications.create!(application_params)
+
+    redirect_to "/pets/#{pet.id}/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :statement)
   end
 end
--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'the applications new page' do
   it 'links you to a new application page from pet index' do
+    shelter = Shelter.create!(foster_program: true, name: "Ye", city: "Yo", rank: 11)
+    dog = shelter.pets.create!(adoptable: true, age: 11, breed: "dog", name: "Sparky")
 
     visit "/pets"
 
     click_on "Start an Adoption Application"
 
-    expect(current_path).to eq("/applications/new")
+    expect(current_path).to eq("/pets/#{dog.id}/applications/new")
   end
-
 end
